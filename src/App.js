7fd4c5f440af3696dac4f75925a386fe2eb9aa95
sophia1215 <code.sophiawang@gmail.com>
2018-09-04 18:48:11 +0800

    Implementing Component: CourseList & Course && Use the CMS back-end Contentful

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import NavBar from "./components/NavBar";
-
+import CourseList from "./components/CourseList";
 
 class App extends Component {
   render() {
     return (
       <div>
        <NavBar />
+       <CourseList />
       </div>
     );
   }
